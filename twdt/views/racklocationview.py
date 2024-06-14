8f8f2e8721a6963b60206c38c60742be5f93bc41
Jason Huan <jason.huan@tollgroup.com>
2024-06-14 11:00:36 +0000

    2024-06-14-1100

--- twdt/views/racklocationview.py
+++ twdt/views/racklocationview.py
@@ -1,11 +1,11 @@
-from typing import cast
+from typing import Any, cast
 
 from django.db.models import QuerySet
 from django.http import HttpRequest, HttpResponse, JsonResponse
 from django.shortcuts import render
 from django.views import View
 
-from twdt.models import RackLocation
+from twdt.models import Rack, RackLocation, Warehouse
 
 
 class RackLocationView(View):
@@ -14,13 +14,28 @@ class RackLocationView(View):
         if not response_type and not request.user.is_authenticated:
             return HttpResponse("", status=401)
 
-        rack_location: QuerySet[RackLocation] = RackLocation.objects.filter(location_id=location_id)
+        rack_location_qs: QuerySet[RackLocation] = RackLocation.objects.filter(location_id=location_id)
+        if not rack_location_qs.count():
+            return HttpResponse("", status=404)
+
+        rack_location_obj: RackLocation = rack_location_qs.get()
+        rack_location: dict[str, Any] = cast(Any, rack_location_qs.values().first())
+
+        rack_location["pallets"] = list(
+            rack_location_obj.pallet_set.order_by("pallet_id").values()
+        )
+        rack_location["rack"] = Rack.objects.filter(id=rack_location["rack_id"]).values().first()
+        rack_location["warehouse"] = Warehouse.objects.filter(id=rack_location["rack"]["warehouse_id"]).values().first()
 
         if response_type == "json":
             return JsonResponse({
                 "data_type": "rack_location",
-                "data": rack_location.values().first(),
+                "data": rack_location,
             })
+
+        import json
+        from django.core.serializers.json import DjangoJSONEncoder # zzz
+        data = json.dumps(rack_location, indent=4, cls=DjangoJSONEncoder)
 
         return render(request, "twdt/rack_location_detail.html", locals())
 
